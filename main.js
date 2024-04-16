c73fcf5735e4373b74ba3f0ae0b8bb225d41a11f
Salem <salemmakansi17@gmail.com>
2024-04-16 16:44:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ let numero = 0;
 document.querySelector(".boton").addEventListener("click",()=>{
     numero +=  1;
     document.querySelector(".cuenta").innerHTML = `<h2>${numero}</h2>`})
+
+
+
+
+document.getElementById("btn_reset").addEventListener("click",()=>{
+    numero=0
+    document.querySelector(".cuenta").innerHTML = `<h2>${numero}</h2>`
+})
